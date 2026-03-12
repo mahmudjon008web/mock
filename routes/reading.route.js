a1eb2a214ee5cea7ef12dafd0244c750475762dc
mahmudjon008web <hamidovahilola420@gmail.com>
2026-03-12 11:49:30 +0500

    swagger yozildi

--- routes/reading.route.js
+++ routes/reading.route.js
@@ -4,14 +4,199 @@ const {isAdmin} = require("../middleware/isAdmin")
 const {protect} = require("../middleware/protected")
 
 
+/**
+ * @swagger
+ * api/reading/all:
+ *   get:
+ *     summary: Barcha readinglarni olish
+ *     tags: [Reading]
+ *     responses:
+ *       200:
+ *         description: Barcha readinglar ro'yxati
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   id:
+ *                     type: integer
+ *                   title:
+ *                     type: string
+ *                   parts:
+ *                     type: array
+ *                     items:
+ *                       type: object
+ *                       properties:
+ *                         id:
+ *                           type: integer
+ *                         part_name:
+ *                           type: string
+ *                         part_texts:
+ *                           type: string
+ *                         part_number:
+ *                           type: integer
+ *                         questions:
+ *                           type: array
+ *                           items:
+ *                             type: object
+ *                             properties:
+ *                               id:
+ *                                 type: integer
+ *                               question_text:
+ *                                 type: string
+ *                               question_type:
+ *                                 type: string
+ *                               options:
+ *                                 type: array
+ *                                 items:
+ *                                   type: object
+ *                                   properties:
+ *                                     id:
+ *                                       type: integer
+ *                                     variant:
+ *                                       type: string
+ *                                     variant_text:
+ *                                       type: string
+ */
 router.get("/all", protect, isAdmin, getAllReadings)
 
+/**
+ * @swagger
+ * api/reading/{id}:
+ *   get:
+ *     summary: Readingni ID orqali olish
+ *     tags: [Reading]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: Reading ID
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: Reading topildi
+ *       404:
+ *         description: Reading topilmadi
+ */
 router.get("/:id", protect, isAdmin, getReadingById)
 
+/**
+ * @swagger
+ * api/reading/post:
+ *   post:
+ *     summary: Yangi reading qo'shish
+ *     tags: [Reading]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - title
+ *               - parts
+ *             properties:
+ *               title:
+ *                 type: string
+ *               parts:
+ *                 type: array
+ *                 items:
+ *                   type: object
+ *                   properties:
+ *                     part_name:
+ *                       type: string
+ *                     part_texts:
+ *                       type: string
+ *                     part_number:
+ *                       type: integer
+ *                     questions:
+ *                       type: array
+ *                       items:
+ *                         type: object
+ *                         properties:
+ *                           question_text:
+ *                             type: string
+ *                           question_type:
+ *                             type: string
+ *                           answers:
+ *                             type: string
+ *                           question_range:
+ *                             type: string
+ *                           extra_data:
+ *                             type: object
+ *                           options:
+ *                             type: array
+ *                             items:
+ *                               type: object
+ *                               properties:
+ *                                 variant:
+ *                                   type: string
+ *                                 variant_text:
+ *                                   type: string
+ *     responses:
+ *       201:
+ *         description: Reading muvaffaqiyatli qo'shildi
+ *       400:
+ *         description: Xato request
+ */
 router.post("/post", protect, isAdmin, postReading)
 
+/**
+ * @swagger
+ * api/reading/update/{id}:
+ *   patch:
+ *     summary: Readingni yangilash
+ *     tags: [Reading]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: Reading ID
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               title:
+ *                 type: string
+ *               parts:
+ *                 type: array
+ *                 items:
+ *                   type: object
+ *     responses:
+ *       200:
+ *         description: Reading muvaffaqiyatli yangilandi
+ *       404:
+ *         description: Reading topilmadi
+ */
 router.patch("/update/:id", protect, isAdmin, updateReadingById)
 
+/**
+ * @swagger
+ * api/reading/delete/{id}:
+ *   delete:
+ *     summary: Readingni o'chirish
+ *     tags: [Reading]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: Reading ID
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: Reading muvaffaqiyatli o'chirildi
+ *       404:
+ *         description: Reading topilmadi
+ */
 router.delete("/delete/:id", protect, isAdmin, deleteReadingByReadingId)
 
 module.exports = router
--- routes/writing.route.js
+++ routes/writing.route.js
@@ -5,14 +5,141 @@ const upload = require("../utils/fileUpload")
 
 const router = require("express").Router()
 
-router.get("/", protect, isAdmin, getTasks)
+/**
+ * @swagger
+ * api/writing/all:
+ *   get:
+ *     summary: Barcha writing olish
+ *     tags: [Writing]
+ *     responses:
+ *       200:
+ *         description: Writing ro'yxati
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   id:
+ *                     type: integer
+ *                   task1_img:
+ *                     type: string
+ *                     example: /uploads/image.png
+ *                   task1_text:
+ *                     type: string
+ *                   task2_text:
+ *                     type: string
+ */
+router.get("/all", protect, isAdmin, getTasks)
 
+/**
+ * @swagger
+ * api/writing/{id}:
+ *   get:
+ *     summary: Writingni ID orqali olish
+ *     tags: [Writing]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: Writing id
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: Writing topildi
+ *       404:
+ *         description: Ma'lumot topilmadi
+ */
 router.get("/:id", protect, isAdmin, getTaskById)
 
+/**
+ * @swagger
+ * api/writing/post:
+ *   post:
+ *     summary: Yangi writing  qo'shish
+ *     tags: [Writing]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         multipart/form-data:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - task1_img
+ *               - task1_text
+ *               - task2_text
+ *             properties:
+ *               task1_img:
+ *                 type: string
+ *                 format: binary
+ *               task1_text:
+ *                 type: string
+ *               task2_text:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: Writing muvaffaqiyatli qo'shildi
+ *       400:
+ *         description: Xato request
+ */
 router.post("/post", protect, isAdmin, upload.single("task1_img"), postTasks)
 
+/**
+ * @swagger
+ * api/writing/update/{id}:
+ *   patch:
+ *     summary: Writing yangilash
+ *     tags: [Writing]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: Writing ID
+ *         schema:
+ *           type: integer
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         multipart/form-data:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               task1_img:
+ *                 type: string
+ *                 format: binary
+ *               task1_text:
+ *                 type: string
+ *               task2_text:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Writing muvaffaqiyatli yangilandi
+ *       404:
+ *         description: Writing topilmadi
+ */
 router.patch("/update/:id", protect, isAdmin, upload.single("task1_img"), updateTask)
 
+/**
+ * @swagger
+ * api/writing/delete/{id}:
+ *   delete:
+ *     summary: Writingni o'chirish
+ *     tags: [Writing]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: Task ID
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: Writing muvaffaqiyatli o'chirildi
+ *       404:
+ *         description: Ma'lumot topilmadi
+ */
 router.delete("/delete/:id", protect, isAdmin, deleteTask)
 
 module.exports = router
